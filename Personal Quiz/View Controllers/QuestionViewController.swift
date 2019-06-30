dafae560bdeb2ffeb9ce3e8948101b78fe2ced95
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2019-06-30 19:47:02 +0300

    UI

--- Personal Quiz/View Controllers/QuestionViewController.swift
+++ Personal Quiz/View Controllers/QuestionViewController.swift
@@ -9,7 +9,46 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var rangeStackView: UIStackView!
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+
+    private var currentQuestion = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        cardView.layer.cornerRadius = 8.0
+        nextQuestion()
+    }
+
+    private func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangeStackView.isHidden = true
+    }
+
+    private func nextQuestion() {
+        updateUI()
+
+        switch currentQuestion {
+        case 0:
+            singleStackView.isHidden = false
+        case 1:
+            multipleStackView.isHidden = false
+        case 2:
+            rangeStackView.isHidden = false
+        default:
+            performSegue(withIdentifier: "showResults", sender: nil)
+        }
+
+        currentQuestion += 1
+    }
+
+    @IBAction func resultsButtonPressed(_ sender: Any) {
+        nextQuestion()
     }
 }
