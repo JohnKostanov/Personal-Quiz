ce0e7e5b527b6fd96a5fd6721b2c0ecb7627feaa
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2019-06-30 14:13:11 +0300

    Init proj

---
+++ Personal Quiz/App/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Personal Quiz
+//
+//  Created by Nikolay Naumenkov on 30/06/2019.
+//  Copyright © 2019 iApps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
