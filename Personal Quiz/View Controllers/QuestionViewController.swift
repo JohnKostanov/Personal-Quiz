8ffad3c231c0693164551131dbe578489b3ad2fc
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2019-07-04 00:15:03 +0300

    Add make UI

--- Personal Quiz/View Controllers/QuestionViewController.swift
+++ Personal Quiz/View Controllers/QuestionViewController.swift
@@ -12,14 +12,13 @@ class QuestionViewController: UIViewController {
     @IBOutlet var cardView: UIView!
 
     @IBOutlet var singleStackView: UIStackView!
-    @IBOutlet var singleButtons: [UIButton]!
 
     @IBOutlet var rangeStackView: UIStackView!
     @IBOutlet var rangedSlider: UISlider!
     @IBOutlet var rangedLabels: [UILabel]!
 
     @IBOutlet var multipleStackView: UIStackView!
-    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleContentStackView: UIStackView!
 
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var progressView: UIProgressView!
@@ -31,14 +30,18 @@ class QuestionViewController: UIViewController {
 
         cardView.layer.cornerRadius = 8.0
 
-        for singleButton in singleButtons {
-            singleButton.layer.cornerRadius = 4.0
-        }
-
         let dataManager = DataManager()
         quizLogic = QuizLogic(questions: dataManager.getAllQuestions())
 
         updateUI()
+    }
+
+    private func nextQuestion() {
+        if (quizLogic.nextQuestion()) {
+            updateUI()
+        } else {
+            goToResultsVC()
+        }
     }
 
     private func updateUI() {
@@ -66,17 +69,21 @@ class QuestionViewController: UIViewController {
 
     private func updateSingleStack(with answers: [Answer]) {
         singleStackView.isHidden = false
+        clearSubviews(view: singleStackView)
 
-        for (button, answer) in zip(singleButtons, answers) {
-            button.setTitle(answer.text, for: [])
+        for answer in answers {
+            let button = makeSigleButton(title: answer.text)
+            singleStackView.addArrangedSubview(button)
         }
     }
 
     private func updateMultipleStack(with answers: [Answer]) {
         multipleStackView.isHidden = false
+        clearSubviews(view: multipleContentStackView)
 
-        for (label, answer) in zip(multipleLabels, answers) {
-            label.text = answer.text
+        for answer in answers {
+            let row = makeMultipleRow(labelText: answer.text)
+            multipleContentStackView.addArrangedSubview(row)
         }
     }
 
@@ -86,12 +93,44 @@ class QuestionViewController: UIViewController {
         rangedLabels.last?.text = answers.last?.text
     }
 
-    private func nextQuestion() {
-        if (quizLogic.nextQuestion()) {
-            updateUI()
-        } else {
-            goToResultsVC()
-        }
+    private func makeSigleButton(title: String) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: [])
+        button.setTitleColor(#colorLiteral(red: 0.5647058824, green: 0.2549019608, blue: 1, alpha: 1), for: [])
+        button.backgroundColor = UIColor.white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15.0)
+        button.layer.cornerRadius = 4.0
+        button.addTarget(self, action: #selector(singleButtonPressed(_:)), for: .touchUpInside)
+        return button
+    }
+
+    private func makeMultipleRow(labelText: String) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 20.0
+
+        let label = UILabel()
+        label.text = labelText
+        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 17.0)
+        label.textColor = UIColor.white
+        label.setContentHuggingPriority(UILayoutPriority(251), for: .horizontal)
+        label.setContentCompressionResistancePriority(UILayoutPriority(750), for: .horizontal)
+
+        let switchView = UISwitch()
+        switchView.setOn(false, animated: false)
+        switchView.setContentHuggingPriority(UILayoutPriority(750), for: .horizontal)
+        switchView.setContentCompressionResistancePriority(UILayoutPriority(751), for: .horizontal)
+
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(switchView)
+
+        return stackView
+    }
+
+    private func clearSubviews(view: UIView) {
+        view.subviews.forEach { $0.removeFromSuperview() }
     }
 
     // MARK: - Navigation
@@ -109,7 +148,7 @@ class QuestionViewController: UIViewController {
     // MARK: - Actions
 
     @IBAction func singleButtonPressed(_ sender: UIButton) {
-        guard let index = singleButtons.firstIndex(of: sender) else {
+        guard let index = singleStackView.arrangedSubviews.firstIndex(of: sender) else {
             return
         }
 
@@ -126,9 +165,9 @@ class QuestionViewController: UIViewController {
         quizLogic.chooseAnswer(at: index)
         nextQuestion()
     }
-
+    
     @IBAction func multipleButtonPressed() {
-        for (index, view) in multipleStackView.arrangedSubviews.enumerated() {
+        for (index, view) in multipleContentStackView.arrangedSubviews.enumerated() {
             guard let stackView = view as? UIStackView else { continue }
             guard let switchView = stackView.arrangedSubviews.last as? UISwitch else { continue }
 
